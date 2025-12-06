42dd955f259b9270457db68c34c19369f03a7c21
Sébastien Weber <seba.weber@gmail.com>
2025-12-06 22:24:15 +0100

    feat: set theme for single widget
    * using qtpy for agnostic qt backend and setting theme for single widget
    
    * Update pyproject.toml
    
    * fix: remove qtpy dependency
    
    * style: format with black and typos
    
    ---------
    
    Co-authored-by: Beat Reichenbach <44111292+beatreichenbach@users.noreply.github.com>

--- qt_themes/__init__.py
+++ qt_themes/__init__.py
@@ -1,3 +1,11 @@
-from ._theme import Theme, get_theme, get_themes, set_theme, update_palette
+from ._theme import (
+    Theme,
+    get_theme,
+    get_themes,
+    set_theme,
+    set_widget_theme,
+    update_palette,
+)
+
 
 __version__ = '0.3.0'
--- qt_themes/_theme.py
+++ qt_themes/_theme.py
@@ -110,7 +110,7 @@ def get_themes() -> dict[str, Theme]:
 
 
 def update_palette(palette: QtGui.QPalette, theme: Theme) -> None:
-    """Set the Theme for the given QPalette."""
+    """Set the theme for the given QPalette."""
 
     # Colors
     highlighted_color = theme.primary
@@ -181,8 +181,8 @@ def update_palette(palette: QtGui.QPalette, theme: Theme) -> None:
 
 def set_theme(theme: Theme | str | None, style: str | None = 'fusion') -> None:
     """
-    Sets the theme and style for the current QApplication.
-    By default, set the Fusion style as it works the best with QPalette ColorRoles.
+    Set the theme and style for the current QApplication.
+    By default, set the fusion style as it works the best with QPalette ColorRoles.
     """
 
     # Set style
@@ -207,6 +207,35 @@ def set_theme(theme: Theme | str | None, style: str | None = 'fusion') -> None:
         application.setProperty(PROPERTY_NAME, theme)
 
 
+def set_widget_theme(
+    widget: QtWidgets.QWidget, theme: Theme | str | None, style: str | None = 'fusion'
+) -> None:
+    """
+    Set the theme and style for the given QWidget.
+    By default, set the fusion style as it works the best with QPalette ColorRoles.
+    """
+
+    # Set style
+    if style:
+        widget.setStyle(QtWidgets.QStyleFactory.create(style))
+
+    # Reset theme
+    if not theme:
+        widget.setPalette(QtGui.QPalette())
+        return
+
+    # Set theme
+    if isinstance(theme, str):
+        theme = get_theme(theme)
+        if not theme:
+            return
+
+    palette = QtGui.QPalette()
+    update_palette(palette, theme)
+    widget.setPalette(palette)
+    widget.setProperty(PROPERTY_NAME, theme)
+
+
 def _load(path: str) -> Theme:
     """
     Return the theme from `path`.
@@ -223,7 +252,7 @@ def _load(path: str) -> Theme:
 
 
 def _get_paths() -> tuple[str, ...]:
-    """Returns all paths to search for themes."""
+    """Return all paths to search for themes."""
 
     paths = [str(importlib.resources.files(qt_themes).joinpath('themes'))]
     if env_path := os.getenv(THEMES):
